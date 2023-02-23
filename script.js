52abf00e9c0bcc855ccc2805545d22948cbfd48e
Priyanka Sharma <priyankashar717@gmail.com>
2023-02-23 16:46:07 +0530

    improved weight converter

--- script.js
+++ script.js
@@ -190,8 +190,53 @@ const data_convertor = () => {
       result = input * 1024;
     }
   }
-  document.getElementById("resultDataCalculator").innerHTML = `= ${result+' '+to
-  }`;
+  document.getElementById("resultDataCalculator").innerHTML = `= ${result + ' ' + to}`;
+  // document.querySelector('h2').textContent = result + ' ' + to;
+}
+
+const weigh_convertor = () => {
+  let input = parseFloat(document.querySelector('#inputweigh').value);
+  let from = document.querySelector('#from').value;
+  let to = document.querySelector('#to').value;
+  let result;
+
+  if (from == to) {
+    result = input;
+  } else if (from == 'kg') {
+    if (to == 'ton') {
+      result = input / 1000;
+    } else if (to == 'g') {
+      result = input * 1000;
+    } else if (to == 'lb') {
+      result = input * 2.20462;
+    }
+  } else if (from == 'ton') {
+    if (to == 'kg') {
+      result = input * 1000;
+    } else if (to == 'g') {
+      result = input * Math.pow(10, 6);
+    } else if (to == 'lb') {
+      result = input * 2204.62;
+    }
+
+  } else if (from == 'g') {
+    if (to == 'kg') {
+      result = input / 1000;
+    } else if (to == 'ton') {
+      result = input / Math.pow(10, 6);
+    } else if (to == 'lb') {
+      result = input * 0.00220462;
+    }
+  } else if (from == 'lb') {
+    if (to == 'kg') {
+      result = input * 0.453592;
+    } else if (to == 'g') {
+      result = input * 453.592;
+    } else if (to == 'ton') {
+      result = input * 0.000453592;
+    }
+  }
+  document.getElementById("resultWeighCalculator").innerHTML = `= ${result + ' ' + to}`;
   // document.querySelector('h2').textContent = result + ' ' + to;
 }
 const changeNumber = () => {
@@ -718,6 +763,14 @@ function fun10() {
   document.querySelector("#calc10").addEventListener('click', function () {
     document.querySelector('#inputdata').value = "";
     document.getElementById('resultDataContainer').innerHTML = "";
+  });
+}
+
+//reset for weight convertor
+function fun11() {
+  document.querySelector("#calc11").addEventListener('click', function () {
+    document.querySelector('#inputweigh').value = "";
+    document.getElementById('resultWeighCalculator').innerHTML = "";
   });
 }
 
